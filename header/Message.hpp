3db12cdbb6310ec96e8fa01f36db2ffe562f8e5e
Matthieu Gillieron <gillieron.matthieu@ik.me>
2026-07-06 14:41:44 +0200

    feat: add message class

---
+++ header/Message.hpp
@@ -0,0 +1,26 @@
+
+#ifndef MESSAGE_HPP
+#define MESSAGE_HPP
+
+#include <vector>
+#include <string>
+
+
+struct Message
+{
+
+	std::string command;
+	std::vector<std::string> param;
+
+
+};
+
+
+
+
+
+
+
+
+
+#endif
