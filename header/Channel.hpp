f78a761740b4ae907d99b73bc16ce1030ceca6fc
Matthieu Gillieron <gillieron.matthieu@ik.me>
2026-07-21 11:32:50 +0200

    feat: channel setting, moderator + cmd

---
+++ header/Channel.hpp
@@ -0,0 +1,76 @@
+
+#ifndef CHANNEL_HPP
+#define CHANNEL_HPP
+
+#include <string>
+#include <vector>
+#include "Client.hpp"
+
+class Channel
+{
+	public:
+		Channel(const std::string &name);
+		~Channel() {};
+
+		// === GETTERS ===
+		std::string getName() const { return _name; }
+		std::string getTopic() const { return _topic; }
+		std::string getKey() const { return _key; }
+		size_t getLimit() const { return _limit; }
+
+		bool isInviteOnly() const { return _inviteOnly; }
+		bool isTopicRestricted() const { return _topicRestricted; }
+		bool hasKey() const { return _hasKey; }
+		bool hasLimit() const { return _hasLimit; }
+
+		// === SETTERS / MODES ===
+		void setTopic(const std::string &topic) { _topic = topic; }
+
+		void setInviteOnly(bool value) { _inviteOnly = value; }
+		void setTopicRestricted(bool value) { _topicRestricted = value; }
+		void setKey(const std::string &key) { _key = key; _hasKey = true; }
+		void removeKey() { _key.clear(); _hasKey = false; }
+		void setLimit(size_t limit) { _limit = limit; _hasLimit = true; }
+		void removeLimit() { _limit = 0; _hasLimit = false; }
+
+		// === MEMBERS ===
+		void addMember(Client *client);
+		void removeMember(Client *client);
+		bool isMember(Client *client) const;
+		size_t getMemberCount() const { return _members.size(); }
+		const std::vector<Client*> &getMembers() const { return _members; }
+		bool isEmpty() const { return _members.empty(); }
+
+		// === MODOS (operateurs de channel) ===
+		void addModo(Client *client);
+		void removeModo(Client *client);
+		bool isModo(Client *client) const;
+
+		// === INVITES (par nickname) ===
+		void addInvite(const std::string &nick);
+		void removeInvite(const std::string &nick);
+		bool isInvited(const std::string &nick) const;
+
+		// === UTILS ===
+		// Envoie msg (+ "\r\n") a tous les membres, sauf 'except' si fourni.
+		void broadcast(const std::string &msg, Client *except = NULL);
+		// Liste des noms pour le RPL_NAMREPLY (353), ops prefixes de '@'.
+		std::string getNamesList() const;
+
+	private:
+		std::string _name;
+		std::string _topic;
+		std::string _key;
+		size_t _limit;
+
+		bool _inviteOnly;
+		bool _topicRestricted;
+		bool _hasKey;
+		bool _hasLimit;
+
+		std::vector<Client*> _members;
+		std::vector<Client*> _modos;
+		std::vector<std::string> _invited;
+};
+
+#endif
---
+++ src/Channel.cpp
@@ -0,0 +1,146 @@
+
+#include "../header/Channel.hpp"
+
+
+Channel::Channel(const std::string &name)
+	: _name(name), _limit(0), _inviteOnly(false),
+	  _topicRestricted(false), _hasKey(false), _hasLimit(false)
+{
+}
+
+
+
+
+// === MEMBERS ===
+
+void Channel::addMember(Client *client)
+{
+	if (isMember(client))
+		return;
+	_members.push_back(client);
+}
+
+void Channel::removeMember(Client *client)
+{
+	for (size_t i = 0; i < _members.size(); i++)
+	{
+		if (_members[i] == client)
+		{
+			_members.erase(_members.begin() + i);
+			break;
+		}
+	}
+	// un membre qui part ne doit plus etre modo
+	removeModo(client);
+}
+
+bool Channel::isMember(Client *client) const
+{
+	for (size_t i = 0; i < _members.size(); i++)
+	{
+		if (_members[i] == client)
+			return true;
+	}
+	return false;
+}
+
+
+
+
+
+// === MODERATOR ===
+
+void Channel::addModo(Client *client)
+{
+	if (isModo(client))
+		return;
+	_modos.push_back(client);
+}
+
+void Channel::removeModo(Client *client)
+{
+	for (size_t i = 0; i < _modos.size(); i++)
+	{
+		if (_modos[i] == client)
+		{
+			_modos.erase(_modos.begin() + i);
+			break;
+		}
+	}
+}
+
+bool Channel::isModo(Client *client) const
+{
+	for (size_t i = 0; i < _modos.size(); i++)
+	{
+		if (_modos[i] == client)
+			return true;
+	}
+	return false;
+}
+
+
+
+
+
+// === INVITES (par nickname) ===
+
+void Channel::addInvite(const std::string &nick)
+{
+	if (isInvited(nick))
+		return;
+	_invited.push_back(nick);
+}
+
+void Channel::removeInvite(const std::string &nick)
+{
+	for (size_t i = 0; i < _invited.size(); i++)
+	{
+		if (_invited[i] == nick)
+		{
+			_invited.erase(_invited.begin() + i);
+			break;
+		}
+	}
+}
+
+bool Channel::isInvited(const std::string &nick) const
+{
+	for (size_t i = 0; i < _invited.size(); i++)
+	{
+		if (_invited[i] == nick)
+			return true;
+	}
+	return false;
+}
+
+
+
+
+
+// === UTILS ===
+
+void Channel::broadcast(const std::string &msg, Client *except)
+{
+	std::string complete = msg + "\r\n";
+	for (size_t i = 0; i < _members.size(); i++)
+	{
+		if (_members[i] == except)
+			continue;
+		_members[i]->appendToOutBuffer(complete);
+	}
+}
+
+std::string Channel::getNamesList() const
+{
+	std::string list;
+	for (size_t i = 0; i < _members.size(); i++)
+	{
+		if (i > 0)
+			list += " ";
+		if (isModo(_members[i]))
+			list += "@";
+		list += _members[i]->getNickName();
+	}
+	return list;
+}
